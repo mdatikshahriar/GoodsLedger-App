b20ca297234c79a616db97a73c5c209427366768
mdatikshahriar <md.atikshahriar728@gmail.com>
2020-01-13 19:31:02 +0600

    2nd commit - Demo QR scanner removed

--- app/src/main/java/com/example/goods_ledger/ConsumerPart/ui/ConsumerHomeFragment.java
+++ app/src/main/java/com/example/goods_ledger/ConsumerPart/ui/ConsumerHomeFragment.java
@@ -1,40 +1,23 @@
 package com.example.goods_ledger.ConsumerPart.ui;
 
-import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.example.goods_ledger.ConsumerPart.ConsumerAccountInfoActivity;
-import com.example.goods_ledger.ConsumerPart.Demo;
 import com.example.goods_ledger.MainActivity;
 import com.example.goods_ledger.R;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
-
-import github.nisrulz.qreader.QRDataListener;
-import github.nisrulz.qreader.QREader;
 
 public class ConsumerHomeFragment extends Fragment {
 
@@ -75,66 +58,12 @@ public class ConsumerHomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                startActivity(new Intent(getActivity(), Demo.class));
-
-//                final Button cameraButton;
-//                ProgressBar progressBar;
-//                final SurfaceView surfaceView;
-//                final QREader qrEader;
-//
-//                addProductsPopup.setContentView(R.layout.popup_addproducts_qrcode_scanner);
-//                addProductsPopup.setCanceledOnTouchOutside(true);
-//
-//                cameraButton = addProductsPopup.findViewById(R.id.popup_addproducts_qrcode_scanner_camera_Button);
-//                progressBar = addProductsPopup.findViewById(R.id.popup_addproducts_qrcode_scanner_Progressbar);
-//                surfaceView = addProductsPopup.findViewById(R.id.popup_addproducts_qrcode_scanner_camera_SurfaceView);
-//
-//                qrEader = new QREader.Builder(getActivity(), surfaceView, new QRDataListener() {
-//                    @Override
-//                    public void onDetected(String data) {
-//                        Log.d("Scanner",data);
-//                    }
-//                }).facing(QREader.BACK_CAM)
-//                        .enableAutofocus(true)
-//                        .height(surfaceView.getHeight())
-//                        .width(surfaceView.getWidth())
-//                        .build();
-//
-//                Dexter.withActivity(getActivity())
-//                        .withPermission(Manifest.permission.CAMERA)
-//                        .withListener(new PermissionListener() {
-//                            @Override
-//                            public void onPermissionGranted(PermissionGrantedResponse response) {
-//                                cameraButton.setOnClickListener(new View.OnClickListener() {
-//                                    @Override
-//                                    public void onClick(View view) {
-//                                        qrEader.start();
-//                                    }
-//                                });
-//                            }
-//
-//                            @Override
-//                            public void onPermissionDenied(PermissionDeniedResponse response) {
-//                                Toast.makeText(getActivity(), "Enable Camera Permission", Toast.LENGTH_SHORT).show();
-//                            }
-//
-//                            @Override
-//                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-//
-//                            }
-//                        }).check();
-//
-//
-//
-//                addProductsPopup.show();
-//                addProductsPopup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             }
         });
 
         checkProductsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), Demo.class));
             }
         });
 
